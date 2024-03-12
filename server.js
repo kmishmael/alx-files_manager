5be1958c1b2ab6606d7afbc2f353abacfcc3cd9e
kmishmael <kmishmael@gmail.com>
2024-03-12 11:39:13 +0300

    fix: start express server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
