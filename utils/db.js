d6d0e00d066e11167c3ca48f91100f71e869f5ce
kmishmael <kmishmael@gmail.com>
2024-03-12 10:45:29 +0300

    chore: create mongodb client

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,34 @@
 #!/usr/bin/node
 
-class DBClient {
-    constructor() {
+const { MongoClient } = require('mongodb');
+const mongo = require('mongodb');
 
-    }
+class DBClient {
+  constructor() {
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    this.databaseUrl = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const dbUrl = `mongodb://${host}:${port}`;
+    this.connected = false;
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    return await this.client.db(this.databaseUrl).collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return await this.client.db(this.databaseUrl).collection('files').countDocuments();
+  }
 }
+
+const dbClient = new DBClient();
+
+export default dbClient;
