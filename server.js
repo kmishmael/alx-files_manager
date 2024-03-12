38081bb7456b22467d3a8471759282182ad98095
kmishmael <kmishmael@gmail.com>
2024-03-12 11:36:55 +0300

    create Express client

---
+++ server.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.use(routes)
