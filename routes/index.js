bb980af61e6686cdc6068783ed19c33646b5a56d
kmishmael <kmishmael@gmail.com>
2024-03-13 18:16:19 +0300

    chore: add user route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@
 
 const { Router } = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
+
+router.get('/users', UsersController.postNew)
 
 module.exports = router;
